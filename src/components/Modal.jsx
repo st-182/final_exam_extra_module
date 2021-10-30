38db509539c56425e70ac1bbcd01d3456e81ab54
st-182 <31894656+st-182@users.noreply.github.com>
2021-10-30 11:02:49 +0300

    modal improved

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
+const StyledSection = styled.section`
+  background: rgba(0, 0, 0, 0.3);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > div {
+    width: 70%;
+    height: 70%;
+    /* padding: 200px; */
+    background: white;
+  }
+`;
 
-const Modal = ({ closeModal }) => {
+const Modal = ({ closeModal, children }) => {
+  // Hooks
+  // -- side effects
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        closeModal();
+      }
+    });
+  }, [closeModal]);
+  // Custom functions
+  const closeOnOverlay = (e) => {
+    if (e.target.dataset.id === "closeLayer") closeModal();
+    return;
+  };
   return ReactDOM.createPortal(
-    <div>
-      <h1>I'am modal'</h1>
-      <button onClick={closeModal}>Close</button>
-    </div>,
+    <StyledSection onClick={closeOnOverlay} data-id="closeLayer">
+      <div>
+        <div>{children}</div>
+        <button onClick={closeModal}>Close</button>
+      </div>
+    </StyledSection>,
     document.getElementById("portal")
   );
 };
